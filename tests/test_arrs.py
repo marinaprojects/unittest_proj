e630264b7a3ddee91918910b57df39a8a72094f9
marinaprojects <155964514+marinaprojects@users.noreply.github.com>
2024-02-13 17:44:32 -0600

    test fixed

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -6,6 +6,8 @@ class TestArrs(unittest.TestCase):
 
     def test_get(self):
         self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
+        self.assertEqual(arrs.get([1, '2', '3'], 1, "test"), '2')
+        self.assertEqual(arrs.get([0], 1, "test"), "test")
         self.assertEqual(arrs.get([], 0, "test"), "test")
 
     def test_slice(self):
